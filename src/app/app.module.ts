f8853fe692497d85041866b55b603348f45ccbdb
garyyb <garyybai@gmail.com>
2017-07-18 20:40:29 +1000

    add router, fix base href

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
+
+const router = RouterModule.forRoot([
+]);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    router
   ],
   providers: [],
   bootstrap: [AppComponent]
